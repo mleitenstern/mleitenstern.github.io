78ccd9984c5395a55298658760e27c2aa41ffb3a
Maximilian Leitenstern <maxi.leitenstern@tum.de>
2026-02-14 15:51:26 +0100

    try to fix rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mleitenstern.github.io',
-  base: 'mleitenstern.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
